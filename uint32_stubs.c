d811515d862ebf5fc5eb38765212078ba97a7c95
Andre Nathan <andre@digirati.com.br>
2010-03-05 14:32:17 -0300

    Removing dead code.

--- uint32_stubs.c
+++ uint32_stubs.c
@@ -158,14 +158,6 @@ uint32_to_int32(value v)
 }
 
 CAMLprim value
-uint32_compare(value v1, value v2)
-{
-    uint32 i1 = Uint32_val(v1);
-    uint32 i2 = Uint32_val(v2);
-    return Val_int((i1 > i2) - (i1 < i2));
-}
-
-CAMLprim value
 uint32_bits_of_float(value vd)
 {
     union { float d; uint32 i; } u;
@@ -182,146 +174,11 @@ uint32_float_of_bits(value vi)
 }
 
 CAMLprim value
-uint32_max_int(value dummy)
+uint32_max_int(void)
 {
     return copy_uint32(UINT32_MAX);
 }
 
-static char *
-parse_sign_and_base(char *p, /*out*/ int *base)
-{
-    if (*p == '-')
-        caml_failwith("Uint32.of_string");
-    *base = 10;
-    if (*p == '0') {
-        switch (p[1]) {
-        case 'x':
-        case 'X':
-            *base = 16;
-            p += 2;
-            break;
-        case 'o':
-        case 'O':
-            *base = 8;
-            p += 2;
-            break;
-        case 'b':
-        case 'B':
-            *base = 2;
-            p += 2;
-            break;
-        }
-    }
-    return p;
-}
-
-static int
-parse_digit(char c)
-{
-    if (c >= '0' && c <= '9')
-        return c - '0';
-    else if (c >= 'A' && c <= 'F')
-        return c - 'A' + 10;
-    else if (c >= 'a' && c <= 'f')
-        return c - 'a' + 10;
-    else
-        return -1;
-}
-
-static intnat
-parse_intnat(value s, int nbits)
-{
-    char *p;
-    uintnat res, threshold;
-    int base, d;
-
-    p = parse_sign_and_base(String_val(s), &base);
-    threshold = ((uintnat) -1) / base;
-    d = parse_digit(*p);
-    if (d < 0 || d >= base) caml_failwith("int_of_string");
-    for (p++, res = d; /*nothing*/; p++) {
-        char c = *p;
-        if (c == '_')
-            continue;
-        d = parse_digit(c);
-        if (d < 0 || d >= base)
-            break;
-        /* Detect overflow in multiplication base * res */
-        if (res > threshold)
-            caml_failwith("uint_of_string");
-        res = base * res + d;
-        /* Detect overflow in addition (base * res) + d */
-        if (res < (uintnat) d)
-            caml_failwith("uint_of_string");
-    }
-    if (p != String_val(s) + caml_string_length(s))
-        caml_failwith("uint_of_string");
-    if (nbits < sizeof(uintnat) * 8 && res >= (uintnat)1 << nbits)
-        caml_failwith("uint_of_string");
-    return((intnat)res);
-}
-
 CAMLprim value
-uint32_of_string(value s)
 {
-    return copy_uint32(parse_intnat(s, 32));
-}
-
-
-#define FORMAT_BUFFER_SIZE 32
-
-static char *
-parse_format(value fmt, char *suffix, char format_string[],
-             char default_format_buffer[], char *conv)
-{
-    int prec;
-    char *p;
-    char lastletter;
-    mlsize_t len, len_suffix;
-
-    /* Copy Caml format fmt to format_string,
-       adding the suffix before the last letter of the format */
-    len = caml_string_length(fmt);
-    len_suffix = strlen(suffix);
-    if (len + len_suffix + 1 >= FORMAT_BUFFER_SIZE)
-        caml_invalid_argument("format_uint: format too long");
-    memmove(format_string, String_val(fmt), len);
-    p = format_string + len - 1;
-    lastletter = *p;
-    /* Compress two-letter formats, ignoring the [lnL] annotation */
-    if (p[-1] == 'l' || p[-1] == 'n' || p[-1] == 'L') p--;
-    memmove(p, suffix, len_suffix);  p += len_suffix;
-    *p++ = lastletter;
-    *p = 0;
-    /* Determine space needed for result and allocate it dynamically if needed */
-    prec = 22 + 5; /* 22 digits for 64-bit number in octal + 5 extra */
-    for (p = String_val(fmt); *p != 0; p++) {
-        if (*p >= '0' && *p <= '9') {
-            prec = atoi(p) + 5;
-            break;
-        }
-    }
-    *conv = lastletter;
-    if (prec < FORMAT_BUFFER_SIZE)
-        return default_format_buffer;
-    else
-        return caml_stat_alloc(prec + 1);
-}
-
-CAMLprim value
-uint32_format(value fmt, value arg)
-{
-    char format_string[FORMAT_BUFFER_SIZE];
-    char default_format_buffer[FORMAT_BUFFER_SIZE];
-    char *buffer;
-    char conv;
-    value res;
-
-    buffer = parse_format(fmt, ARCH_INT32_PRINTF_FORMAT,
-                          format_string, default_format_buffer, &conv);
-    sprintf(buffer, format_string, Uint32_val(arg));
-    res = caml_copy_string(buffer);
-    if (buffer != default_format_buffer)
-        caml_stat_free(buffer);
-    return res;
 }
--- uint64_stubs.c
+++ uint64_stubs.c
@@ -180,14 +180,6 @@ uint64_to_float(value v)
 }
 
 CAMLprim value
-uint64_compare(value v1, value v2)
-{
-    uint64_t i1 = Uint64_val(v1);
-    uint64_t i2 = Uint64_val(v2);
-    return Val_int((i1 > i2) - (i1 < i2));
-}
-
-CAMLprim value
 uint64_bits_of_float(value vd)
 {
     union { float d; uint64_t i; } u;
@@ -204,144 +196,11 @@ uint64_float_of_bits(value vi)
 }
 
 CAMLprim value
-uint64_max_int(value dummy)
+uint64_max_int(void)
 {
     return copy_uint64(UINT64_MAX);
 }
 
-static char *
-parse_sign_and_base(char *p, /*out*/ int *base)
-{
-    if (*p == '-')
-        caml_failwith("Uint64.of_string");
-    *base = 10;
-    if (*p == '0') {
-        switch (p[1]) {
-        case 'x':
-        case 'X':
-            *base = 16;
-            p += 2;
-            break;
-        case 'o':
-        case 'O':
-            *base = 8;
-            p += 2;
-            break;
-        case 'b':
-        case 'B':
-            *base = 2;
-            p += 2;
-            break;
-        }
-    }
-    return p;
-}
-
-static int
-parse_digit(char c)
-{
-    if (c >= '0' && c <= '9')
-        return c - '0';
-    else if (c >= 'A' && c <= 'F')
-        return c - 'A' + 10;
-    else if (c >= 'a' && c <= 'f')
-        return c - 'a' + 10;
-    else
-        return -1;
-}
-
 CAMLprim value
-uint64_of_string(value s)
 {
-    char * p;
-    uint64_t max_uint64 = UINT64_MAX;
-    uint64_t max_int64  = INT64_MAX;
-    uint64_t res, threshold;
-    int base, d;
-
-    p = parse_sign_and_base(String_val(s), &base);
-    threshold = max_uint64 / (uint64_t)base;
-    res = max_uint64 % (uint64_t)base;
-    d = parse_digit(*p);
-    if (d < 0 || d >= base)
-        caml_failwith("Uint64.of_string");
-    res = (uint64_t)d;
-    for (p++; /*nothing*/; p++) {
-        char c = *p;
-        if (c == '_')
-            continue;
-        d = parse_digit(c);
-        if (d < 0 || d >= base)
-            break;
-        /* Detect overflow in multiplication base * res */
-        if (threshold < res)
-            caml_failwith("int_of_string");
-        res = (uint64_t)base * (uint64_t)res + (uint64_t)d;
-        /* Detect overflow in addition (base * res) + d */
-        if (res < (uint64_t)d)
-            caml_failwith("Uint64.of_string");
-    }
-    if (p != String_val(s) + caml_string_length(s))
-        caml_failwith("Uint64.of_string");
-    if (base == 10 && max_int64 < res)
-        caml_failwith("Uint64.of_string");
-    return caml_copy_int64(res);
-}
-
-#define FORMAT_BUFFER_SIZE 32
-
-static char *
-parse_format(value fmt, char *suffix, char format_string[],
-             char default_format_buffer[], char *conv)
-{
-    int prec;
-    char *p;
-    char lastletter;
-    mlsize_t len, len_suffix;
-
-    /* Copy Caml format fmt to format_string,
-       adding the suffix before the last letter of the format */
-    len = caml_string_length(fmt);
-    len_suffix = strlen(suffix);
-    if (len + len_suffix + 1 >= FORMAT_BUFFER_SIZE)
-        caml_invalid_argument("format_uint: format too long");
-    memmove(format_string, String_val(fmt), len);
-    p = format_string + len - 1;
-    lastletter = *p;
-    /* Compress two-letter formats, ignoring the [lnL] annotation */
-    if (p[-1] == 'l' || p[-1] == 'n' || p[-1] == 'L') p--;
-    memmove(p, suffix, len_suffix);  p += len_suffix;
-    *p++ = lastletter;
-    *p = 0;
-    /* Determine space needed for result and allocate it dynamically if needed */
-    prec = 22 + 5; /* 22 digits for 64-bit number in octal + 5 extra */
-    for (p = String_val(fmt); *p != 0; p++) {
-        if (*p >= '0' && *p <= '9') {
-            prec = atoi(p) + 5;
-            break;
-        }
-    }
-    *conv = lastletter;
-    if (prec < FORMAT_BUFFER_SIZE)
-        return default_format_buffer;
-    else
-        return caml_stat_alloc(prec + 1);
-}
-
-CAMLprim value
-uint64_format(value fmt, value arg)
-{
-    char format_string[FORMAT_BUFFER_SIZE];
-    char default_format_buffer[FORMAT_BUFFER_SIZE];
-    char *buffer;
-    char conv;
-    value res;
-
-    buffer = parse_format(fmt, ARCH_INT64_PRINTF_FORMAT,
-                          format_string, default_format_buffer, &conv);
-    sprintf(buffer, format_string, Uint64_val(arg));
-    res = caml_copy_string(buffer);
-    if (buffer != default_format_buffer)
-        caml_stat_free(buffer);
-    return res;
 }
