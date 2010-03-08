470cb52cc96b79383bd5738f23a5bced58cab4c2
Andre Nathan <andre@digirati.com.br>
2010-03-08 09:35:47 -0300

    CAMLizing stubs.

--- uint32_stubs.c
+++ uint32_stubs.c
@@ -48,140 +48,161 @@ struct custom_operations uint32_ops = {
 CAMLprim value
 copy_uint32(uint32 i)
 {
+    CAMLparam0();
     value res = caml_alloc_custom(&uint32_ops, 4, 0, 1);
     Uint32_val(res) = i;
-    return res;
+    CAMLreturn (res);
 }
 
 CAMLprim value
 uint32_add(value v1, value v2)
 {
-    return copy_uint32(Uint32_val(v1) + Uint32_val(v2));
+    CAMLparam2(v1, v2);
+    CAMLreturn (copy_uint32(Uint32_val(v1) + Uint32_val(v2)));
 }
 
 CAMLprim value
 uint32_sub(value v1, value v2)
 {
-    return copy_uint32(Uint32_val(v1) - Uint32_val(v2));
+    CAMLparam2(v1, v2);
+    CAMLreturn (copy_uint32(Uint32_val(v1) - Uint32_val(v2)));
 }
 
 CAMLprim value
 uint32_mul(value v1, value v2)
 {
-    return copy_uint32(Uint32_val(v1) * Uint32_val(v2));
+    CAMLparam2(v1, v2);
+    CAMLreturn (copy_uint32(Uint32_val(v1) * Uint32_val(v2)));
 }
 
 CAMLprim value
 uint32_div(value v1, value v2)
 {
+    CAMLparam2(v1, v2);
     uint32 divisor = Uint32_val(v2);
     if (divisor == 0)
         caml_raise_zero_divide();
-    return copy_uint32(Uint32_val(v1) / divisor);
+    CAMLreturn (copy_uint32(Uint32_val(v1) / divisor));
 }
 
 CAMLprim value
 uint32_mod(value v1, value v2)
 {
+    CAMLparam2(v1, v2);
     uint32 divisor = Uint32_val(v2);
     if (divisor == 0)
         caml_raise_zero_divide();
-    return copy_uint32(Uint32_val(v1) % divisor);
+    CAMLreturn (copy_uint32(Uint32_val(v1) % divisor));
 }
 
 CAMLprim value
 uint32_and(value v1, value v2)
 {
-    return copy_uint32(Uint32_val(v1) & Uint32_val(v2));
+    CAMLparam2(v1, v2);
+    CAMLreturn (copy_uint32(Uint32_val(v1) & Uint32_val(v2)));
 }
 
 CAMLprim value
 uint32_or(value v1, value v2)
 {
-    return copy_uint32(Uint32_val(v1) | Uint32_val(v2));
+    CAMLparam2(v1, v2);
+    CAMLreturn (copy_uint32(Uint32_val(v1) | Uint32_val(v2)));
 }
 
 CAMLprim value
 uint32_xor(value v1, value v2)
 {
-    return copy_uint32(Uint32_val(v1) ^ Uint32_val(v2));
+    CAMLparam2(v1, v2);
+    CAMLreturn (copy_uint32(Uint32_val(v1) ^ Uint32_val(v2)));
 }
 
 CAMLprim value
 uint32_shift_left(value v1, value v2)
 {
-    return copy_uint32(Uint32_val(v1) << Int_val(v2));
+    CAMLparam2(v1, v2);
+    CAMLreturn (copy_uint32(Uint32_val(v1) << Int_val(v2)));
 }
 
 CAMLprim value
 uint32_shift_right(value v1, value v2)
 {
-    return copy_uint32(Uint32_val(v1) >> Int_val(v2));
+    CAMLparam2(v1, v2);
+    CAMLreturn (copy_uint32(Uint32_val(v1) >> Int_val(v2)));
 }
 
 CAMLprim value
 uint32_of_int(value v)
 {
+    CAMLparam1(v);
     long l = Long_val(v);
-    return copy_uint32(Long_val(v));
+    CAMLreturn (copy_uint32(Long_val(v)));
 }
 
 CAMLprim value
 uint32_to_int(value v)
 {
-    return Val_long(Uint32_val(v));
+    CAMLparam1(v);
+    CAMLreturn (Val_long(Uint32_val(v)));
 }
 
 CAMLprim value
 uint32_of_float(value v)
 {
-    return copy_uint32((uint32)Double_val(v));
+    CAMLparam1(v);
+    CAMLreturn (copy_uint32((uint32)Double_val(v)));
 }
 
 CAMLprim value
 uint32_to_float(value v)
 {
-    return caml_copy_double((double)Uint32_val(v));
+    CAMLparam1(v);
+    CAMLreturn (caml_copy_double((double)Uint32_val(v)));
 }
 
 CAMLprim value
 uint32_of_int32(value v)
 {
+    CAMLparam1(v);
     long l = Long_val(v);
-    return copy_uint32((uint32)Int32_val(v));
+    CAMLreturn (copy_uint32((uint32)Int32_val(v)));
 }
 
 CAMLprim value
 uint32_to_int32(value v)
 {
-    return caml_copy_int32(Uint32_val(v));
+    CAMLparam1(v);
+    CAMLreturn (caml_copy_int32(Uint32_val(v)));
 }
 
 CAMLprim value
-uint32_bits_of_float(value vd)
+uint32_bits_of_float(value v)
 {
+    CAMLparam1(v);
     union { float d; uint32 i; } u;
-    u.d = Double_val(vd);
-    return copy_uint32(u.i);
+    u.d = Double_val(v);
+    CAMLreturn (copy_uint32(u.i));
 }
 
 CAMLprim value
-uint32_float_of_bits(value vi)
+uint32_float_of_bits(value v)
 {
+    CAMLparam1(v);
     union { float d; uint32 i; } u;
-    u.i = Uint32_val(vi);
-    return caml_copy_double(u.d);
+    u.i = Uint32_val(v);
+    CAMLreturn (caml_copy_double(u.d));
 }
 
 CAMLprim value
 uint32_max_int(void)
 {
-    return copy_uint32(UINT32_MAX);
+    CAMLparam0();
+    CAMLreturn (copy_uint32(UINT32_MAX));
 }
 
 CAMLprim value
 uint32_init_custom_ops(void)
 {
+    CAMLparam0();
     caml_register_custom_operations(&uint32_ops);
-    return Val_unit;
+    CAMLreturn (Val_unit);
 }
--- uint64_stubs.c
+++ uint64_stubs.c
@@ -48,162 +48,187 @@ struct custom_operations uint64_ops = {
 CAMLprim value
 copy_uint64(uint64 i)
 {
+    CAMLparam0();
     value res = caml_alloc_custom(&uint64_ops, 8, 0, 1);
     Uint64_val(res) = i;
-    return res;
+    CAMLreturn (res);
 }
 
 CAMLprim value
 uint64_add(value v1, value v2)
 {
-    return copy_uint64(Uint64_val(v1) + Uint64_val(v2));
+    CAMLparam2(v1, v2);
+    CAMLreturn (copy_uint64(Uint64_val(v1) + Uint64_val(v2)));
 }
 
 CAMLprim value
 uint64_sub(value v1, value v2)
 {
-    return copy_uint64(Uint64_val(v1) - Uint64_val(v2));
+    CAMLparam2(v1, v2);
+    CAMLreturn (copy_uint64(Uint64_val(v1) - Uint64_val(v2)));
 }
 
 CAMLprim value
 uint64_mul(value v1, value v2)
 {
-    return copy_uint64(Uint64_val(v1) * Uint64_val(v2));
+    CAMLparam2(v1, v2);
+    CAMLreturn (copy_uint64(Uint64_val(v1) * Uint64_val(v2)));
 }
 
 CAMLprim value
 uint64_div(value v1, value v2)
 {
+    CAMLparam2(v1, v2);
     uint64 divisor = Uint64_val(v2);
     if (divisor == 0)
         caml_raise_zero_divide();
-    return copy_uint64(Uint64_val(v1) / divisor);
+    CAMLreturn (copy_uint64(Uint64_val(v1) / divisor));
 }
 
 CAMLprim value
 uint64_mod(value v1, value v2)
 {
+    CAMLparam2(v1, v2);
     uint64 divisor = Uint64_val(v2);
     if (divisor == 0)
         caml_raise_zero_divide();
-    return copy_uint64(Uint64_val(v1) % divisor);
+    CAMLreturn (copy_uint64(Uint64_val(v1) % divisor));
 }
 
 CAMLprim value
 uint64_and(value v1, value v2)
 {
-    return copy_uint64(Uint64_val(v1) & Uint64_val(v2));
+    CAMLparam2(v1, v2);
+    CAMLreturn (copy_uint64(Uint64_val(v1) & Uint64_val(v2)));
 }
 
 CAMLprim value
 uint64_or(value v1, value v2)
 {
-    return copy_uint64(Uint64_val(v1) | Uint64_val(v2));
+    CAMLparam2(v1, v2);
+    CAMLreturn (copy_uint64(Uint64_val(v1) | Uint64_val(v2)));
 }
 
 CAMLprim value
 uint64_xor(value v1, value v2)
 {
-    return copy_uint64(Uint64_val(v1) ^ Uint64_val(v2));
+    CAMLparam2(v1, v2);
+    CAMLreturn (copy_uint64(Uint64_val(v1) ^ Uint64_val(v2)));
 }
 
 CAMLprim value
 uint64_shift_left(value v1, value v2)
 {
-    return copy_uint64(Uint64_val(v1) << Int_val(v2));
+    CAMLparam2(v1, v2);
+    CAMLreturn (copy_uint64(Uint64_val(v1) << Int_val(v2)));
 }
 
 CAMLprim value
 uint64_shift_right(value v1, value v2)
 {
-    return copy_uint64(Uint64_val(v1) >> Int_val(v2));
+    CAMLparam2(v1, v2);
+    CAMLreturn (copy_uint64(Uint64_val(v1) >> Int_val(v2)));
 }
 
 CAMLprim value
 uint64_of_int(value v)
 {
-    return copy_uint64(Long_val(v));
+    CAMLparam1(v);
+    CAMLreturn (copy_uint64(Long_val(v)));
 }
 
 CAMLprim value
 uint64_to_int(value v)
 {
-    return Val_long(Uint64_val(v));
+    CAMLparam1(v);
+    CAMLreturn (Val_long(Uint64_val(v)));
 }
 
 CAMLprim value
 uint64_of_int32(value v)
 {
-    return copy_uint64((uint32)Int32_val(v));
+    CAMLparam1(v);
+    CAMLreturn (copy_uint64((uint32)Int32_val(v)));
 }
 
 CAMLprim value
 uint64_to_int32(value v)
 {
-    return caml_copy_int32(Uint64_val(v));
+    CAMLparam1(v);
+    CAMLreturn (caml_copy_int32(Uint64_val(v)));
 }
 
 CAMLprim value
 uint64_of_nativeint(value v)
 {
-    return copy_uint64((uint32)Nativeint_val(v));
+    CAMLparam1(v);
+    CAMLreturn (copy_uint64((uint32)Nativeint_val(v)));
 }
 
 CAMLprim value
 uint64_to_nativeint(value v)
 {
-    return caml_copy_nativeint((intnat)Uint64_val(v));
+    CAMLparam1(v);
+    CAMLreturn (caml_copy_nativeint((intnat)Uint64_val(v)));
 }
 
 CAMLprim value
 uint64_of_int64(value v)
 {
-    return copy_uint64((uint64)Int64_val(v));
+    CAMLparam1(v);
+    CAMLreturn (copy_uint64((uint64)Int64_val(v)));
 }
 
 CAMLprim value
 uint64_to_int64(value v)
 {
-    return caml_copy_int64(Uint64_val(v));
+    CAMLparam1(v);
+    CAMLreturn (caml_copy_int64(Uint64_val(v)));
 }
 
 CAMLprim value
 uint64_of_float(value v)
 {
-    return copy_uint64((uint64)Double_val(v));
+    CAMLparam1(v);
+    CAMLreturn (copy_uint64((uint64)Double_val(v)));
 }
 
 CAMLprim value
 uint64_to_float(value v)
 {
-    return caml_copy_double((double)Uint64_val(v));
+    CAMLparam1(v);
+    CAMLreturn (caml_copy_double((double)Uint64_val(v)));
 }
 
 CAMLprim value
-uint64_bits_of_float(value vd)
+uint64_bits_of_float(value v)
 {
+    CAMLparam1(v);
     union { float d; uint64 i; } u;
-    u.d = Double_val(vd);
-    return copy_uint64(u.i);
+    u.d = Double_val(v);
+    CAMLreturn (copy_uint64(u.i));
 }
 
 CAMLprim value
-uint64_float_of_bits(value vi)
+uint64_float_of_bits(value v)
 {
+    CAMLparam1(v);
     union { float d; uint64 i; } u;
-    u.i = Uint64_val(vi);
-    return caml_copy_double(u.d);
+    u.i = Uint64_val(v);
+    CAMLreturn (caml_copy_double(u.d));
 }
 
 CAMLprim value
 uint64_max_int(void)
 {
-    return copy_uint64(UINT64_MAX);
+    CAMLparam0();
+    CAMLreturn (copy_uint64(UINT64_MAX));
 }
 
 CAMLprim value
 uint64_init_custom_ops(void)
 {
+    CAMLparam0();
     caml_register_custom_operations(&uint64_ops);
-    return Val_unit;
+    CAMLreturn (Val_unit);
 }
