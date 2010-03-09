ff1f6f607813266674e0439725e68f917b5e9857
Andre Nathan <andre@digirati.com.br>
2010-03-09 10:48:58 -0300

    String conversion spec for Uint128.

--- spec/uint128.ml
+++ spec/uint128.ml
@@ -1,4 +1,14 @@
 #load "uint64.cma";;
 #load "uint128.cma";;
+#load "str.cma";;
 open Uint128;;
 #use "spec/common.ml";;
+
+describe "A 128-bit integer" do
+  it "should be converted from strings correctly" do
+    forall (char_in_range '0' '2') d .
+      forall (string_of ~length:(fun () -> 38) digit) s .
+        let str = Str.replace_first (Str.regexp "^0+") "" s in
+        (to_string (of_string str)) should = str
+  done
+done
