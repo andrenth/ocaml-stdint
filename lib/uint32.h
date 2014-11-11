fa028f1d050ecaed69c9b334e7fc18a2dd0cd2c5
Andre Nathan <andre@digirati.com.br>
2014-11-11 18:58:18 -0200

    Fix typo

--- lib/uint32.h
+++ lib/uint32.h
@@ -1,7 +1,7 @@
 #ifndef OCAML_UINT32_H
 #define OCAML_UINT32_H
 
-#define Uint32_val(v) (*((utin32_t *)Data_custom_val(v)))
+#define Uint32_val(v) (*((uint32_t *)Data_custom_val(v)))
 
-CAMLextern value copy_uint32(utin32_t i);
+CAMLextern value copy_uint32(uint32_t i);
 #endif
