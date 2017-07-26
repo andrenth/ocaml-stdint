46dfd1af2391fc5819280e9eb087ba19a713bd19
Cedric Cellier <rixed@happyleptic.org>
2017-07-26 12:36:35 +0200

    Add (failing) test
    Shows that there is a bug when converting from u16 to u32

--- spec/uint32.ml
+++ spec/uint32.ml
@@ -1,3 +1,10 @@
 #load "stdint.cma";;
 open Stdint.Uint32;;
 #use "spec/common.ml";;
+
+describe "An uint32" do
+  it "should convert to/from an uint8 correctly" do
+    forall (int_in_range 0 255) x .
+      (Stdint.Uint8.of_int x |> of_uint8 |> to_int) should = x
+  done;
+done
